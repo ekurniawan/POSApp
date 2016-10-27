6a9b8f8de1d39567c2c258374056a796ddef3f08
erick <erick@DESKTOP-7DP9Q9N>
2016-10-27 14:39:09 +0700

    menambahkan pemasok DAL

--- DAL/PemasokDAL.cs
+++ DAL/PemasokDAL.cs
@@ -18,5 +18,47 @@ namespace DAL
             return ConfigurationManager
                 .ConnectionStrings["POSDbConnectionString"].ConnectionString;
         }
+
+        public IEnumerable<Pemasok> GetAll()
+        {
+            using (SqlConnection conn = new SqlConnection(GetConnString()))
+            {
+                List<Pemasok> listPemasok = new List<Pemasok>();
+                string strSql = @"select * from Pemasok 
+                                  order by Nama";
+
+                SqlCommand cmd = new SqlCommand(strSql, conn);
+                try
+                {
+                    conn.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if(dr.HasRows)
+                    {
+                        while(dr.Read())
+                        {
+                            listPemasok.Add(new Pemasok
+                            {
+                                KodePemasok = Convert.ToInt32(dr["KodePemasok"]),
+                                Nama = dr["Nama"].ToString(),
+                                Alamat = dr["Alamat"].ToString(),
+                                Telp = dr["Telp"].ToString()
+                            });
+                        }
+                    }
+                    dr.Close();
+                    return listPemasok;
+                }
+                catch (SqlException sqlEx)
+                {
+                    throw new Exception(sqlEx.Message);
+                }
+                finally
+                {
+                    
+                    cmd.Dispose();
+                    conn.Close();
+                }
+            }
+        }
     }
 }
