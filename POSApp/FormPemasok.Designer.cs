e7b34d6337fcc97373b17a705241658ab57ae853
erick <erick@DESKTOP-7DP9Q9N>
2016-10-27 15:37:40 +0700

    menambahkan form pemasok

--- POSApp/FormPemasok.Designer.cs
+++ POSApp/FormPemasok.Designer.cs
@@ -29,6 +29,22 @@
         private void InitializeComponent()
         {
             this.dgvPemasok = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtKode = new System.Windows.Forms.TextBox();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtAlamat = new System.Windows.Forms.TextBox();
+            this.txtTelp = new System.Windows.Forms.TextBox();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPemasok)).BeginInit();
             this.SuspendLayout();
             // 
@@ -36,27 +52,200 @@
             // 
             this.dgvPemasok.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvPemasok.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.dgvPemasok.Location = new System.Drawing.Point(0, 112);
+            this.dgvPemasok.Location = new System.Drawing.Point(0, 180);
             this.dgvPemasok.Name = "dgvPemasok";
-            this.dgvPemasok.Size = new System.Drawing.Size(551, 176);
+            this.dgvPemasok.Size = new System.Drawing.Size(461, 207);
             this.dgvPemasok.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kode :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nama :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Alamat :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Telp :";
+            // 
+            // txtKode
+            // 
+            this.txtKode.Location = new System.Drawing.Point(82, 6);
+            this.txtKode.Name = "txtKode";
+            this.txtKode.Size = new System.Drawing.Size(100, 20);
+            this.txtKode.TabIndex = 5;
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(82, 35);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(203, 20);
+            this.txtNama.TabIndex = 6;
+            // 
+            // txtAlamat
+            // 
+            this.txtAlamat.Location = new System.Drawing.Point(82, 64);
+            this.txtAlamat.Name = "txtAlamat";
+            this.txtAlamat.Size = new System.Drawing.Size(230, 20);
+            this.txtAlamat.TabIndex = 7;
+            // 
+            // txtTelp
+            // 
+            this.txtTelp.Location = new System.Drawing.Point(82, 90);
+            this.txtTelp.Name = "txtTelp";
+            this.txtTelp.Size = new System.Drawing.Size(143, 20);
+            this.txtTelp.TabIndex = 8;
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(271, 130);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(40, 23);
+            this.btnFirst.TabIndex = 9;
+            this.btnFirst.Text = "<<";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(317, 130);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(40, 23);
+            this.btnPrev.TabIndex = 10;
+            this.btnPrev.Text = "<";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(363, 130);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(40, 23);
+            this.btnNext.TabIndex = 11;
+            this.btnNext.Text = ">";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(409, 130);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(40, 23);
+            this.btnLast.TabIndex = 12;
+            this.btnLast.Text = ">>";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(82, 130);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(41, 23);
+            this.btnNew.TabIndex = 13;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(168, 130);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(41, 23);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(124, 130);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(41, 23);
+            this.btnEdit.TabIndex = 15;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(210, 130);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(50, 23);
+            this.btnDelete.TabIndex = 16;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
             // 
             // FormPemasok
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(551, 288);
+            this.ClientSize = new System.Drawing.Size(461, 387);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.txtTelp);
+            this.Controls.Add(this.txtAlamat);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.txtKode);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dgvPemasok);
             this.Name = "FormPemasok";
             this.Text = "FormPemasok";
             this.Load += new System.EventHandler(this.FormPemasok_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvPemasok)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgvPemasok;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtKode;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtAlamat;
+        private System.Windows.Forms.TextBox txtTelp;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
     }
 }
--- POSApp/FormPemasok.cs
+++ POSApp/FormPemasok.cs
@@ -15,18 +15,83 @@ namespace POSApp
 {
     public partial class FormPemasok : Form
     {
+        private BindingSource bs;
+
         public FormPemasok()
         {
             InitializeComponent();
         }
 
+        #region InisialisasiBinding
+        private void HapusBinding()
+        {
+            txtKode.DataBindings.Clear();
+            txtNama.DataBindings.Clear();
+            txtAlamat.DataBindings.Clear();
+            txtTelp.DataBindings.Clear();
+        }
+
+        private void TambahBinding()
+        {
+            HapusBinding();
+            txtKode.DataBindings.Add("Text", bs, "KodePemasok");
+            txtNama.DataBindings.Add("Text", bs, "Nama");
+            txtAlamat.DataBindings.Add("Text", bs, "Alamat");
+            txtTelp.DataBindings.Add("Text", bs, "Telp");
+        }
+        #endregion
+
+        #region Inisialisasi Kontrol
+        private void InisialisasiAwal()
+        {
+            foreach(var ctr in this.Controls)
+            {
+                if(ctr is TextBox)
+                {
+                    var myTxt = ctr as TextBox;
+                    myTxt.Enabled = false;
+                }
+                else if(ctr is Button)
+                {
+                    var myBtn = ctr as Button;
+                    myBtn.Enabled = true;
+                }
+            }
+
+            btnSave.Enabled = false;
+        }
+        #endregion
+
         private void FormPemasok_Load(object sender, EventArgs e)
         {
             PemasokDAL pemasokDAL = new PemasokDAL();
-            var results = pemasokDAL.GetAll();
 
+            bs = new BindingSource();
+            bs.DataSource = pemasokDAL.GetAll();
+            TambahBinding();
+            InisialisasiAwal();
             
-            dgvPemasok.DataSource = results;
+            dgvPemasok.DataSource = bs;
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            bs.MoveFirst();
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            bs.MovePrevious();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            bs.MoveNext();
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            bs.MoveLast();
         }
     }
 }
