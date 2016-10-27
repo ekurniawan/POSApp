114261b5807d8582d9115850f10ed12b0f4c2feb
erick <erick@DESKTOP-7DP9Q9N>
2016-10-27 14:26:03 +0700

    menambahkan connection string

--- DAL/PemasokDAL.cs
+++ DAL/PemasokDAL.cs
@@ -7,10 +7,16 @@ using System.Threading.Tasks;
 using BO;
 using System.Data;
 using System.Data.SqlClient;
+using System.Configuration;
 
 namespace DAL
 {
     public class PemasokDAL
     {
+        public string GetConnString()
+        {
+            return ConfigurationManager
+                .ConnectionStrings["POSDbConnectionString"].ConnectionString;
+        }
     }
 }
