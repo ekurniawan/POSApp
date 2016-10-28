54fa155768654e35badc2a4b97e4e34731bcb51c
erick <erick@DESKTOP-7DP9Q9N>
2016-10-28 11:13:51 +0700

    menambahkan dapper pada BarangDAL

---
+++ DAL/BarangDAL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BO;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using System.Configuration;
+
+namespace DAL
+{
+    public class BarangDAL
+    {
+        public string GetConnString()
+        {
+            return ConfigurationManager
+                .ConnectionStrings["POSDbConnectionString"].ConnectionString;
+        }
+
+        public IEnumerable<Barang> GetAll()
+        {
+            using (SqlConnection conn = new SqlConnection(GetConnString()))
+            {
+                string strSql = @"select * from Barang order by Nama asc";
+                var results = conn.Query<Barang>(strSql, conn);
+                return results;
+            }
+        }
+    }
+}
