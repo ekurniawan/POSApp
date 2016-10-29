1c96eda4dd5506bcffc6f083b7edd138edf9884c
erick <erick@DESKTOP-7DP9Q9N>
2016-10-29 09:59:45 +0700

    menambahkan method createnota dan updatenota pembelian DAL

--- DAL/NotaPembelianDAL.cs
+++ DAL/NotaPembelianDAL.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using BO;
 using Dapper;
 using System.Data.SqlClient;
+using System.Configuration;
 
 namespace DAL
 {
@@ -18,6 +19,48 @@ namespace DAL
                 .ConnectionStrings["POSDbConnectionString"].ConnectionString;
         }
 
+        public int GetNoNotaPembelian(NotaPembelian notaBeli)
+        {
+            using (SqlConnection conn = new SqlConnection(GetConnString()))
+            {
+                string strSql = @"insert into NotaPembelian(KodePemasok,Tanggal) 
+                              values(@KodePemasok,@Tanggal);select @@identity;";
+                var param = new
+                {
+                    KodePemasok = notaBeli.KodePemasok,
+                    Tanggal = notaBeli.Tanggal
+                };
 
+                var result = Convert.ToInt32(conn.ExecuteScalar(strSql, param));
+
+                return result;
+            }
+               
+        }
+
+        public void Update(NotaPembelian notaBeli)
+        {
+            using (SqlConnection conn = new SqlConnection(GetConnString()))
+            {
+                string strSql = @"update NotaPembelian set KodePemasok=@KodePemasok,
+                                  Tanggal=@Tanggal where KodeNotaBeli=@KodeNotaBeli";
+
+                var param = new
+                {
+                    KodePemasok = notaBeli.KodePemasok,
+                    Tanggal = notaBeli.Tanggal,
+                    KodeNotaBeli = notaBeli.KodeNotaBeli
+                };
+
+                try
+                {
+                    conn.Execute(strSql, param);
+                }
+                catch (SqlException sqlEx)
+                {
+                    throw new Exception(sqlEx.Number + " " + sqlEx.Message);
+                }
+            }
+        }
     }
 }
